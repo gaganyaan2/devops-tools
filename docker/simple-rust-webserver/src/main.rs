80e9ca780812b92a18d0e0a0e0e933da0ff496b2
koolwithk <alokmauryaa@gmail.com>
2021-12-13 20:07:03 +0530

    rust fixed

--- docker/simple-rust-webserver/src/main.rs
+++ docker/simple-rust-webserver/src/main.rs
@@ -1,30 +1,46 @@
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::net::{TcpStream, TcpListener};
+use std::io::{Read, Write};
+use std::thread;
 
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
+fn handle_read(mut stream: &TcpStream) {
+    let mut buf = [0u8 ;4096];
+    match stream.read(&mut buf) {
+        Ok(_) => {
+            let req_str = String::from_utf8_lossy(&buf);
+            println!("{}", req_str);
+            },
+        Err(e) => println!("Unable to read stream: {}", e),
     }
 }
 
-use std::fs;
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+fn handle_write(mut stream: TcpStream) {
+    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>hola rust</body></html>\r\n";
+    match stream.write(response) {
+        Ok(_) => println!("Response sent"),
+        Err(e) => println!("Failed sending response: {}", e),
+    }
+}
 
-    let contents = fs::read_to_string("hola.html").unwrap();
+fn handle_client(stream: TcpStream) {
+    handle_read(&stream);
+    handle_write(stream);
+}
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    println!("Listening for connections on port {}", 7878);
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Unable to connect: {}", e);
+            }
+        }
+    }
 }
