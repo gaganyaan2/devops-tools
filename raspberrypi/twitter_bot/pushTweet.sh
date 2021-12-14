7108e77f15ca6cf375ead049a901c4ae2ef070db
koolwithk <alokmauryaa@gmail.com>
2021-12-14 13:15:15 +0530

    fixed sensor broke this time

--- raspberrypi/twitter_bot/pushTweet.sh
+++ raspberrypi/twitter_bot/pushTweet.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-
-sleep 5
-t_and_h=$(python3 /opt/cron/get_temp_dht11.py)
-
-temprerature=$(echo $t_and_h | awk -F ',' '{print $1}')
-humidity=$(echo $t_and_h | awk -F ',' '{print $2}')
-
 unixtime=$(date +%s)
 
-if [[ "$temprerature" == "" ]]
-then
-/usr/bin/docker run -i --rm tbot python twitterBot.py "Sorry, My sensor broke this time:$unixtime :("
-else
+while [[ "$temprerature" == "" ]]
+do
+sleep 5
+t_and_h=$(python3 /opt/cron/get_temp_dht11.py)
+temprerature=$(echo $t_and_h | awk -F ',' '{print $1}')
+humidity=$(echo $t_and_h | awk -F ',' '{print $2}')
+echo "temprerature: $temprerature"
+done
+
 /usr/bin/docker run -i --rm tbot python twitterBot.py "Mumbai, temperature=$temprerature C, humidity=$humidity% - by pibot with dht11 sensor at $unixtime #raspberrypi"
-fi
